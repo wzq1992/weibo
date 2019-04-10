0c9642fd763daa65ca2d2cd5613a57fe850f3954
wzqyyc@yeah.net <wzqyyc@yeah.net>
2019-04-10 10:00:06 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
