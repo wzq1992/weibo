1526e467d1092ed91bdab331d681b3154b9d9c90
wzqyyc@yeah.net <wzqyyc@yeah.net>
2019-04-12 11:18:15 +0000

    Updated database configuration

--- app/helpers.php
+++ app/helpers.php
@@ -8,6 +8,7 @@ function get_db_config()
         return $db_config = [
             'connection' => 'pgsql',
             'host' => $url["host"],
+            'port' => $url['port'],
             'database'  => substr($url["path"], 1),
             'username'  => $url["user"],
             'password'  => $url["pass"],
--- config/database.php
+++ config/database.php
@@ -34,7 +34,7 @@ return [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => $db_config['host'],
-            'port'     => env('DB_PORT', '5432'),
+            'port'     => $db_config['port'],
             'database' => $db_config['database'],
             'username' => $db_config['username'],
             'password' => $db_config['password'],
