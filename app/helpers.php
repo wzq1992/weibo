6ca2e25e2e11b2d3f1de3ed7dfc45777be286165
wzqyyc@yeah.net <wzqyyc@yeah.net>
2019-04-12 12:34:36 +0000

    Updated database configuration

--- app/helpers.php
+++ app/helpers.php
@@ -3,7 +3,7 @@
 function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
-        $url = parse_url(getenv('DATABSE_URL'));
+        $url = parse_url(getenv('DATABASE_URL'));
 
         return $db_config = [
             'connection' => 'pgsql',
